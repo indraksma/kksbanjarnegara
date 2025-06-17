57bf5f6294fdd61493b002dadbf358fdc21f2ffe
Naufal Akbar Pinasthika <nauakbar76@gmail.com>
2025-06-17 11:20:37 +0700

    tambah halaman home + config tailwind

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+
+use App\Livewire\HomePage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomePage::class)->name('homepage');
+
+
 Route::middleware('auth')->get('/home', function () {
     return view('welcome');
 });
