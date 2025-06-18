8762ca1894e6755fe2d73aa4390e11c362e70236
Indra Kusuma <indra.ksma14@gmail.com>
2025-06-18 14:36:02 +0700

    DB Structure & Resource Admin

---
+++ app/Livewire/Admin/File.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+
+class File extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.file');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,8 @@ Route::get('/', HomePage::class)->name('homepage');
 Route::middleware('auth')->get('/home', function () {
     return view('welcome');
 });
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', App\Livewire\Admin\Dashboard::class)->name('dashboard');
+    Route::get('/opd', App\Livewire\Admin\Opd::class)->name('opd');
+    Route::get('/files', App\Livewire\Admin\File::class)->name('files');
+});
