ec3036a04eb51fa9f5ae4d1ea31ed15874d264c4
Naufal Akbar Pinasthika <nauakbar76@gmail.com>
2025-06-23 13:52:22 +0700

    redesign homepage

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 
 use App\Livewire\HomePage;
+use App\Livewire\Pages\Kelembagaan;
+use App\Livewire\Pages\Profile;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class)->name('homepage');
+Route::get('/profil', Profile::class)->name('profile');
+Route::get('/kelembagaan', Kelembagaan::class)->name('kelembagaan');
 
 
 Route::middleware('auth')->get('/home', function () {
